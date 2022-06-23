1c529e31159557df544ef45b6584ce9545e14841
Simply4K <108091684+Simply4K@users.noreply.github.com>
2022-06-23 16:00:41 -0500

    Update python-learning.py

---
+++ python-learning.py
@@ -0,0 +1,17 @@
+from lib2to3.pytree import BasePattern
+
+
+ass = 2578
+
+print(ass)
+simply4k = "deez nuts"
+
+print(simply4k)
+
+urlocalgrapeisgayasfnocapjustfactezlol = 100
+
+print(urlocalgrapeisgayasfnocapjustfactezlol)
+
+coin = "pp"
+
+print(coin)
