ac955f100bd7e681a1e12660664e95342b8aadd9
Simply4K <108091684+Simply4K@users.noreply.github.com>
2022-06-23 16:34:09 -0500

    Update python-learning.py

--- python-learning.py
+++ python-learning.py
@@ -1,4 +1,5 @@
 from lib2to3.pytree import BasePattern
+from re import A
 
 
 ass = 2578
@@ -15,3 +16,44 @@ print(urlocalgrapeisgayasfnocapjustfactezlol)
 coin = "pp"
 
 print(coin)
+
+ez = int(15)
+
+print(ez)
+
+x = float(9)
+
+print(ez)
+
+print(coin)
+
+print(x)
+
+g = "coding"
+
+h = 'coding'
+
+print("coding")
+print('coding')
+
+A = "bruh"
+a = "ice"
+
+print(A)
+print(a)
+
+a = ("Hello world!")
+
+print(len(a))
+
+print(10 > 11)
+
+a = 100
+
+b = 100
+
+if b > a:
+    print("b is not greater than a")
+else:
+    print("b is not greater than a")
+
